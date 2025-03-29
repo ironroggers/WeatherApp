c9e1b9dfcb6f2cafbb732c080697e8fe56242649
ironroggers <97803759+ironroggers@users.noreply.github.com>
2025-03-29 19:40:59 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    RAPID_API_KEY: '460c0f106amsh2d56625e1acfd4ap1e7896jsne35966c556b6', // Get this from RapidAPI
+    RAPID_API_KEY: 'REPLACE_WITH_API_KEY', // Get this from RapidAPI
     RAPID_API_HOST: 'wft-geo-db.p.rapidapi.com',
-    WEATHER_API_KEY : "7G67RGG6TJ46MHQR5XF3FRCRK"
+    WEATHER_API_KEY : "REPLACE_WITH_API_KEY"
 }; 
